0b17f33c797cca8ade769eaeed6d97e7ab4fa49e
Tony Chopp <tonychopp@gmail.com>
2015-10-16 16:27:41 -0700

    update stripe
    stripe key info

--- stripeconfig.php
+++ stripeconfig.php
@@ -3,8 +3,8 @@
 require_once('stripe/vendor/autoload.php');
 
 $stripe = array(
-  "secret_key"      => "sk_live_H1YwPT9PCcmwtwHBpAgXOsw3",
-  "publishable_key" => "pk_live_Cjta64iHYng2Ztpmh6Pl8loe"
+  "secret_key"      => "",
+  "publishable_key" => ""
 );
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
